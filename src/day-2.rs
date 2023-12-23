e51ebbf19dae1da34d710883fe48668862bd5586
Josep Marcello <jspmarcello@live.com>
2023-12-23 22:00:27 +0700

    day-2: part two

--- src/day-2.rs
+++ src/day-2.rs
@@ -1,10 +1,16 @@
 use std::fs;
 
-fn parse_line(line: &str) -> Option<(u16, bool)> {
+fn parse_line(line: &str, is_part_1: bool) -> Option<(i32, bool)> {
     let (id_part, sets_part) = line.split_once(":")?;
 
     let (_, id) = id_part.split_at(5);
-    let id = id.parse::<u16>().expect("Unable to parse id");
+
+    // for part 1
+    let id = id.parse::<i32>().expect("Unable to parse id");
+
+    // for part 2
+    let mut part_2_product = 0;
+    let (mut red_max, mut blue_max, mut green_max) = (i32::MIN, i32::MIN, i32::MIN);
 
     let sets = sets_part.split(";");
     for set in sets {
@@ -14,18 +20,24 @@ fn parse_line(line: &str) -> Option<(u16, bool)> {
             let count = count.parse::<i32>().expect("Unable to parse cube number");
             match color.to_ascii_lowercase().as_str() {
                 "blue" => {
-                    if count > 14 {
+                    if is_part_1 && count > 14 {
                         return Some((id, false));
+                    } else if !is_part_1 && count > blue_max {
+                        blue_max = count;
                     }
                 }
                 "green" => {
-                    if count > 13 {
+                    if is_part_1 && count > 13 {
                         return Some((id, false));
+                    } else if !is_part_1 && count > green_max {
+                        green_max = count;
                     }
                 }
                 "red" => {
-                    if count > 12 {
+                    if is_part_1 && count > 12 {
                         return Some((id, false));
+                    } else if !is_part_1 && count > red_max {
+                        red_max = count;
                     }
                 }
                 _ => {}
@@ -33,15 +45,23 @@ fn parse_line(line: &str) -> Option<(u16, bool)> {
         }
     }
 
-    Some((id, true))
+    if !is_part_1 {
+        part_2_product = red_max * blue_max * green_max;
+    }
+
+    if is_part_1 {
+        Some((id, true))
+    } else {
+        Some((part_2_product, true))
+    }
 }
 
-fn solve(input: &String) {
+fn solve(input: &String, is_part_1: bool) {
     let line_iter = input.split("\n");
     let mut sum = 0;
     for line in line_iter {
-        if let Some((id, possible)) = parse_line(line) {
-            if possible { sum += id }
+        if let Some((res, possible)) = parse_line(line, is_part_1) {
+            if possible { sum += res }
         }
     }
 
@@ -50,5 +70,6 @@ fn solve(input: &String) {
 
 fn main() {
     let input = fs::read_to_string("inputs/day2.txt").expect("unable to read file");
-    solve(&input);
+    solve(&input, true);
+    solve(&input, false);
 }
